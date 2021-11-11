61a654f4f48bfae75019abe7058a03e92098dd91
Origho-precious <Origho9@gmail.com>
2021-11-11 23:39:08 +0100

    fix: updating contract ABI

--- src/data/abi.js
+++ src/data/abi.js
@@ -3,9 +3,6 @@ const contractAbi = [
 		inputs: [],
 		stateMutability: "nonpayable",
 		type: "constructor",
-		constant: undefined,
-		payable: undefined,
-		signature: "constructor",
 	},
 	{
 		inputs: [],
@@ -13,29 +10,32 @@ const contractAbi = [
 		outputs: [],
 		stateMutability: "payable",
 		type: "function",
-		constant: undefined,
-		payable: true,
-		signature: "0xe97dcb62",
 	},
 	{
 		inputs: [],
 		name: "getPlayers",
-		outputs: [[Object]],
+		outputs: [
+			{
+				internalType: "address[]",
+				name: "",
+				type: "address[]",
+			},
+		],
 		stateMutability: "view",
 		type: "function",
-		constant: true,
-		payable: undefined,
-		signature: "0x8b5b9ccc",
 	},
 	{
 		inputs: [],
 		name: "manager",
-		outputs: [[Object]],
+		outputs: [
+			{
+				internalType: "address",
+				name: "",
+				type: "address",
+			},
+		],
 		stateMutability: "view",
 		type: "function",
-		constant: true,
-		payable: undefined,
-		signature: "0x481c6a75",
 	},
 	{
 		inputs: [],
@@ -43,20 +43,28 @@ const contractAbi = [
 		outputs: [],
 		stateMutability: "nonpayable",
 		type: "function",
-		constant: undefined,
-		payable: undefined,
-		signature: "0x5d495aea",
 	},
 	{
-		inputs: [[Object]],
+		inputs: [
+			{
+				internalType: "uint256",
+				name: "",
+				type: "uint256",
+			},
+		],
 		name: "players",
-		outputs: [[Object]],
+		outputs: [
+			{
+				internalType: "address",
+				name: "",
+				type: "address",
+			},
+		],
 		stateMutability: "view",
 		type: "function",
-		constant: true,
-		payable: undefined,
-		signature: "0xf71d96cb",
 	},
 ];
+
+export const contractAddress = 0x3a5e124d69c1789a97baa632089067ebe94147b8;
 
 export default contractAbi;
