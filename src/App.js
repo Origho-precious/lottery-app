e31b423477b0d950f1a1e4217c9a405de150142c
Origho-precious <Origho9@gmail.com>
2021-11-11 23:40:37 +0100

    feat: rendering manager of lottery address

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
+import { useEffect, useState } from "react";
+import localContract from "./configs/lottery";
 import web3Config from "./configs/web3";
 
 const App = () => {
-	console.log(web3Config().eth.getAccounts().then(console.log));
-	return <div>Hi</div>;
+	const [manager, setManager] = useState("");
+
+	useEffect(() => {
+		const getManager = async () => {
+			try {
+				const accounts = await web3Config.eth.getAccounts();
+				console.log(accounts);
+
+				const res = await localContract.methods.manager().call();
+
+				console.log(res);
+
+				setManager(res);
+			} catch (error) {
+				console.log(error);
+			}
+		};
+
+		getManager();
+	}, []);
+
+	return (
+		<div>
+			<h2>Lottery Contract</h2>
+			<p>This Contract is managed by {manager}</p>
+		</div>
+	);
 };
 
 export default App;
